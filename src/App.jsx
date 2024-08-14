0a7f77472256b6cabb144e46a5858125e09d537a
Batuhan <92650261+Batuhan210@users.noreply.github.com>
2024-08-15 01:55:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
     
       useEffect(() => {
         if (allBought) {
-          Alert("Alışveriş Tamamlandı");
+          alert("Shopping Completed");
         }
       }, [products, allBought]);
     
